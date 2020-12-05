a94a133938c153833997d5d641141d386958b9f5
Daniel Kurek <daniel.kurek.dev@gmail.com>
2020-12-05 18:41:29 +0100

    Basic Data model

--- todoList/models.py
+++ todoList/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ToDoTask(models.Model):
+    task_title = models.CharField(max_length=255)
+    create_date = models.DateTimeField('date created', auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    task_description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False, blank=True)
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50)
+    tag_color = models.CharField(max_length=9) # color as #AARRGGBB
+
+class TaskTags(models.Model):
+    task = models.ForeignKey(ToDoTask, on_delete=models.DO_NOTHING)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
--- todoList/views.py
+++ todoList/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import ToDoTask
 
 # Create your views here.
+def index(request):
+    latest_tasks = ToDoTask.objects.order_by('-create_date')[:5]
+    output = ', '.join([t.task_title for t in latest_tasks])
+    return HttpResponse(output)
+def detail(request, task_id):
+    return HttpResponse("You're looking at task %s." % task_id)
