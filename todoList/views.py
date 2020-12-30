a4435f6278a31877e44b879ca738788b2a0b3326
Daniel Kurek <daniel.kurek.dev@gmail.com>
2020-12-30 08:53:46 +0100

    Pagination and hide completed

--- todoList/views.py
+++ todoList/views.py
@@ -1,14 +1,39 @@
-from django.http.response import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 from .models import ToDoTask
 from .forms import TaskForm
 
+def __parseInt(val, defaultValue = None):
+    try:
+        val = int(val)
+    except ValueError:
+        return defaultValue
+    return val
+
 # Create your views here.
 def index(request):
-    tasks = ToDoTask.objects.order_by('completed','-create_date')[:5]
-    return render(request, 'todoList/taskList.html', {'tasks':tasks})
+    showCompleted = request.GET.get("showCompleted", "")
+    if showCompleted == "true":
+        showCompleted = True
+    else:
+        showCompleted = False
+       
+    tasks = ToDoTask.objects
+    if showCompleted == False:
+        tasks = tasks.filter(completed=False)
+    tasks = tasks.order_by('completed','-create_date')
+    
+    paginator = Paginator(tasks, 15, orphans=5)
+    pageNumber = request.GET.get("page")
+    page = paginator.get_page(pageNumber)
+    return render(request, 'todoList/taskList.html', 
+      {
+        'showCompleted': showCompleted,
+        'showCompletedParam': "&showCompleted=true" if showCompleted else "",
+        'page': page
+      })
 
 def detail(request, task_id):
     return HttpResponse("You're looking at task %s." % task_id)
