12d81dae49b43e2bc5c28bc99ebb7e4f7ac52510
Daniel Kurek <daniel.kurek.dev@gmail.com>
2021-01-05 18:41:10 +0100

    Minor changes

--- todoList/urls.py
+++ todoList/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add-task/', views.input, name='add-task'),
     path('add-tag/', views.addTag, name='add-tag'),
     path('added/', views.added, name='added'),
-    path('task/<int:task_id>/', views.detail, name='detail'),
+    path('task/<int:task_id>/', views.detail, name='task-detail'),
     path('task/<int:task_id>/complete', views.complete, name='task-complete'),
 ]
