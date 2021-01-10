1c00e72f28e872291b842ea947d08a07883f0177
Daniel Kurek <daniel.kurek.dev@gmail.com>
2021-01-10 09:28:28 +0100

    Bug fixes

--- todoList/admin.py
+++ todoList/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ToDoTask, Tag
+from .models import TaskList, ToDoTask, Tag
 
 # Register your models here.
 admin.site.register(ToDoTask)
 admin.site.register(Tag)
+admin.site.register(TaskList)
